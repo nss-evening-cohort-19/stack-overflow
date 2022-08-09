3f08f73d3f1169fabfd5a0eadc89cd4378ba02f2
NickScrimale <91919098+NickScrimale@users.noreply.github.com>
2022-08-08 19:02:33 -0500

    changed navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,29 +1,84 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+// import React, { useState } from 'react';
 import Link from 'next/link';
-import {
-  Navbar, Container, Nav, Button,
-} from 'react-bootstrap';
+
 import { signOut } from '../utils/auth';
 
 export default function NavBar() {
+  // const [questions] = useState([]);
+  // const [filteredResults, setFilteredResults] = useState([]);
+  // const [searchInput, setSearchInput] = useState('');
+
+  // const searchItems = (searchValue) => {
+  //   setSearchInput(searchValue);
+  //   if (searchInput !== '') {
+  //     const filteredData = questions.filter((question) => Object.values(question).join('').toLowerCase().includes(searchInput.toLowerCase()));
+  //     setFilteredResults(filteredData);
+  //   } else { setFilteredResults(questions); }
+  // };
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container>
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+      <div className="container-fluid">
         <Link passHref href="/">
-          <Navbar.Brand>CHANGE ME</Navbar.Brand>
+          <a className="navbar-brand" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01">
+            Stack-OverFill
+          </a>
         </Link>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
-            <Link passHref href="/">
-              <Nav.Link>Home</Nav.Link>
-            </Link>
-            <Button variant="danger" onClick={signOut}>Sign Out</Button>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul className="navbar-nav me-auto">
+            <li className="nav-item">
+              <Link passHref href="/">
+                <a className="nav-link">
+                  Home
+                </a>
+              </Link>
+              <Link passHref href="/">
+                <a className="nav-link">
+                  Questions
+                </a>
+              </Link>
+              <Link passHref href="/">
+                <a className="nav-link">
+                  Tags
+                </a>
+              </Link>
+              <Link passHref href="/">
+                <a className="nav-link">
+                  Users
+                </a>
+              </Link>
+              <Link passHref href="/">
+                <a className="nav-link">
+                  Companies
+                </a>
+              </Link>
+            </li>
+            <button type="button" className="btn btn-danger" onClick={signOut}>
+              Sign Out
+            </button>
+          </ul>
+        </div>
+        {/* <form inLine>
+          <form icon="search" placeholder="Search Questions" onChange={(e) => searchItems(e.target.value)} />
+          {searchInput.length > 1 ? (
+            <div className="d-flex flex-wrap">
+              {filteredResults.map((question) => (
+                <QuestionCard key={question.firebaseKey} questionObj={question} onUpdate={getAllTheQuestions} />
+              ))}
+            </div>
+          ) : (
+            <div className="d-flex flex-wrap">
+              {questions.map((question) => (
+                <QuestionCard key={question.firebaseKey} questionObj={question} onUpdate={getAllTheQuestions} />
+              ))}
+            </div>
+          )}
+        </form> */}
+      </div>
+    </nav>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import { Button } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
 
@@ -17,9 +16,9 @@ function Home() {
     >
       <h1>Hello {user.displayName}! </h1>
       <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
+      <button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
         Sign Out
-      </Button>
+      </button>
     </div>
   );
 }
