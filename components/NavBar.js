dd530c14f7446d19d5fdd8ad83e56d9dd8acd77f
NickScrimale <91919098+NickScrimale@users.noreply.github.com>
2022-08-10 19:48:17 -0500

    added search bar

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,54 +1,62 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import { Button } from 'react-bootstrap';
-
+import {
+  Button, Container, Form, Nav,
+} from 'react-bootstrap';
+import QuestionCard from './QuestionCard';
 import { signOut } from '../utils/auth';
+import { getQuestions } from '../api/questionData';
 
 export default function NavBar() {
-  // const [questions] = useState([]);
-  // const [filteredResults, setFilteredResults] = useState([]);
-  // const [searchInput, setSearchInput] = useState('');
+  const [questions] = useState([]);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
 
-  // const searchItems = (searchValue) => {
-  //   setSearchInput(searchValue);
-  //   if (searchInput !== '') {
-  //     const filteredData = questions.filter((question) => Object.values(question).join('').toLowerCase().includes(searchInput.toLowerCase()));
-  //     setFilteredResults(filteredData);
-  //   } else { setFilteredResults(questions); }
-  // };
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== '') {
+      const filteredData = questions.filter((question) => Object.values(question).join('').toLowerCase().includes(searchInput.toLowerCase()));
+      setFilteredResults(filteredData);
+    } else { setFilteredResults(questions); }
+  };
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark">
-      <div className="container-fluid">
+    <NavBar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
         <Link passHref href="/">
-          <a className="navbar-brand" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01">
-            Stack-OverFill
-          </a>
+          <NavBar.Brand>Meme Roster</NavBar.Brand>
         </Link>
-        <Button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon" />
-        </Button>
-
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <ul className="navbar-nav me-auto">
-            <li className="nav-item">
-              <Link passHref href="/">
-                <a className="nav-link">
-                  Home
-                </a>
-              </Link>
-              <Link passHref href="/questions/new">
-                <a className="nav-link">
-                  Add a Question
-                </a>
-              </Link>
-            </li>
-            <Button type="button" className="btn btn-danger" onClick={signOut}>
-              Sign Out
-            </Button>
-          </ul>
-        </div>
-      </div>
-    </nav>
+        <NavBar.Toggle aria-controls="responsive-navbar-nav" />
+        <NavBar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
+            <Link passHref href="/">
+              <Nav.Link>Home</Nav.Link>
+            </Link>
+            <Link passHref href="/team">
+              <Nav.Link>Meme Team</Nav.Link>
+            </Link>
+            <Link passHref href="/teamStuff/new">
+              <Nav.Link>Add a meme</Nav.Link>
+            </Link>
+            <Button variant="danger" onClick={signOut}>Sign Out</Button>
+            <Form.Control icon="search" placeholder="Search Questions" onChange={(e) => searchItems(e.target.value)} />
+            {searchInput.length > 1 ? (
+              <div className="d-flex flex-wrap">
+                {filteredResults.map((question) => (
+                  <QuestionCard key={question.firebaseKey} questionObj={question} onUpdate={getQuestions} />
+                ))}
+              </div>
+            ) : (
+              <div className="d-flex flex-wrap">
+                {questions.map((question) => (
+                  <QuestionCard key={question.firebaseKey} questionObj={question} onUpdate={getQuestions} />
+                ))}
+              </div>
+            )}
+          </Nav>
+        </NavBar.Collapse>
+      </Container>
+    </NavBar>
   );
 }
